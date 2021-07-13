b3c413fe0aa243942348894ef6d0600aeae13ab2
kennedybryant <kennedy_marisa96@yahoo.com>
2021-07-13 19:07:15 -0400

    Task 5 Complete

--- index.js
+++ index.js
@@ -204,11 +204,11 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(km){
+  return km * 0.621371;
 }
 
-
+console.log('task 5a', miles(2));
 
 //Task 5b - Feet to CM
 /*
@@ -218,11 +218,11 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm / 30.48; 
 }
 
-
+console.log('task 5b', feet(160));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
